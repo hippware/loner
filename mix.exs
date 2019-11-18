20defd6bd741731e0e0fe4e8fc66147e12766afc
Bernard Duggan <bernard@hippware.com>
2019-11-18 17:53:29 +1100

    Add some metadata and publish initial version

--- mix.exs
+++ mix.exs
@@ -1,16 +1,34 @@
 defmodule Loner.MixProject do
   use Mix.Project
 
+  @version "0.1.0"
+
   def project do
     [
       app: :loner,
-      version: "0.1.0",
+      version: @version,
       elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
+      description: description(),
+      package: package(),
+      docs: docs(),
       deps: deps(),
-      aliases: [
-        # Required for LocalCluster
-        test: "test --no-start"
+      source_url: "https://github.com/hippware/loner",
+      test_coverage: [tool: ExCoveralls],
+      preferred_cli_env: [
+        coveralls: :test,
+        "coveralls.detail": :test,
+        "coveralls.html": :test
+      ],
+      aliases: aliases(),
+      dialyzer: [
+        flags: [
+          :error_handling,
+          :race_conditions,
+          :underspecs,
+          :unknown,
+          :unmatched_returns
+        ]
       ]
     ]
   end
@@ -29,14 +47,46 @@ defmodule Loner.MixProject do
       {:credo_naming, "~> 0.3", only: [:dev, :test], runtime: false},
       {:dialyxir, "~> 1.0.0-rc.7", only: :dev, runtime: false},
       {:eventually, "~> 1.1", only: :test, runtime: false},
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false},
+      {:excoveralls, "~> 0.10", only: :test},
       # TODO: Back to upstream once logger config fix is merged
       # (https://github.com/whitfin/local-cluster/pull/12)
       # {:local_cluster, "~> 1.0.4", only: :test, runtime: false},
       {:local_cluster,
-       github: "hippware/local-cluster",
-       branch: "fix-remote-log-level",
-       only: :test,
-       runtime: false}
+        github: "hippware/local-cluster",
+        branch: "fix-remote-log-level",
+        only: :test,
+        runtime: false}
+    ]
+  end
+
+  defp aliases() do
+    [
+      # Required for LocalCluster
+      test: "test --no-start"
+    ]
+  end
+
+  defp description do
+    """
+    Loner provides a simple method for creating a registered, supervised
+    singleton process within a multi-node cluster with the help of Horde.
+    """
+  end
+
+  defp package do
+    [
+      maintainers: ["Bernard Duggan"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/hippware/loner"}
+    ]
+  end
+
+  defp docs do
+    [
+      source_ref: "v#\{@version\}",
+      main: "readme",
+      extras: ["README.md"],
     ]
   end
 end
